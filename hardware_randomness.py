30eb2785758cf3b2fb925f05fb9a577b405f231a
ra312 <akylzhanov.r@gmail.com>
2020-01-21 16:47:03 +0000

    added LARS

--- hardware_randomness.py
+++ hardware_randomness.py
@@ -1,12 +1,21 @@
 #!usr/local/bin python3
 import numpy as np
-
+import pandas as pd
 def cryptorand(n):
     with open('/dev/random','rb') as rnd:
         a = np.frombuffer(rnd.read(n*4), dtype=np.uint32) >> 5
         b = np.frombuffer(rnd.read(n*4), dtype=np.uint32) >> 6
         return (a * 67108864.0 + b) / 9007199254740992.0
 
+def generate_random_points(n, p):
+  """
+  use hardware entropy to generate random points n points in p-dimensional 
+  Euclidean space form 
+  """
+  x = np.frombuffer(os.urandom(4*n*p), dtype = np.uint32)
+  x = x.reshape(n,p)
+  x = [row.tolist() for row in x]
+  return x
 
 if __name__ == '__main__':
 
